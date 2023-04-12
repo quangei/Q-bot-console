2e335befab1bff229f77d9e74ae2f4f615913f84
quangei <107247419+quangei@users.noreply.github.com>
2023-04-13 01:05:01 +0700

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,69 @@
+@echo off
+rem Q Bot Console by quangei
+rem https://github.com/qsef1256/DiaBot/
+setlocal
+
+node -v 2> Nul
+if not "%ERRORLEVEL%" == "0" goto errNode
+
+cd %~dp0
+set ver= 1.0.0
+set link= https://github.com/qsef1256/DiaBot/
+title Q-bot-console Installer %ver%
+echo.
+echo Q-bot-console Installer by quangei
+echo Version: %ver%
+echo Link: %link%
+echo.
+echo This installer installs Q-bot modules to the %~dp0.
+echo Enter Y if you want to install, otherwise N.
+set /p yn=[Y/N] : 
+if /i "%yn%" == "Y" goto install
+goto stop
+
+:install
+echo Installing Modules...
+echo Pls waiting install modules, it can be long
+call npm install mineflayer
+call npm install mineflayer-pathfinder
+call npm install mineflayer-web-inventory
+call npm install mineflayer-pvp
+call npm install mineflayer-armor-manager
+call npm install opn
+call npm install readline
+call npm install readline-sync
+
+if not "%ERRORLEVEL%" == "0" goto errInstall
+
+:finish
+echo.
+echo Q Bot Console Module Installer
+echo Version: %ver%
+echo.
+echo Installation finished
+echo Run start.bat to run Q-bot-console.
+echo.
+echo Contact: %link%
+echo.
+pause
+goto stop
+
+:errNode
+echo No Node.js installation detected. 
+echo Please install or repair Node.js.
+echo.
+echo Contact: %link%
+pause
+goto stop
+
+:errInstall
+echo There was a problem installing diabot.
+echo ERRORLEVEL: %ERRORLEVEL%
+echo.
+echo Contact: %link%
+pause
+goto stop
+
+:stop
+endlocal
+exit
